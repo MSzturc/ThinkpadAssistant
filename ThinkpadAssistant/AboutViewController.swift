e65402e98f8d82f14f21110a37401e1da090f32d
MSzturc <MSzturc@googlemail.com>
2020-05-14 19:59:55 +0200

    bugfix: AboutView does not show

--- ThinkpadAssistant/AboutViewController.swift
+++ ThinkpadAssistant/AboutViewController.swift
@@ -15,8 +15,18 @@ class AboutViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
          if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
             self.versionLabel.stringValue = NSLocalizedString("Version", comment: "") + " " + version
         }
+        NSApp.activate(ignoringOtherApps: true)
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: NSApplication.willResignActiveNotification, object: nil)
+        
     }
+    
+    @objc func appMovedToBackground() {
+        self.view.window!.close()
+    }
+    
 }
